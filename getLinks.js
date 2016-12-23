5641f1ae43b91f5da379cfb36f5d7bb3a148cc54
Peter Froud <pfroud@gmail.com>
2016-12-23 02:06:10 -0800

    Added lots of doc comments

--- getLinks.js
+++ getLinks.js
@@ -1,10 +1,15 @@
 "use strict";
 localStorage.clear();
 
+/**
+ * Changes the page to a list of links for the user to open.
+ * As the user runs readPage.js on each page, builds a table of game info as comma-separated values.
+ */
 function main() {
     var rows = $$("a.search_result_row");
     var games = [];
 
+    // Store game info in an array
     rows.forEach(function (row) {
         games.push({
             name: row.querySelector("span.title").innerHTML,
@@ -12,18 +17,20 @@ function main() {
             id: row.href.split("/")[4] //this is the game's Steam ID
         });
     });
-    // see if i can get away without style type and open body
+
+    // Wipe the page and add some CSS
     document.writeln("<head><style>" +
         "p{line-height:50%; font-family:sans-serif;} span{font-weight:bold; color:green; visibility:hidden;}" +
         "</style></head>");
 
+    // Write links for each game
     games.forEach(function (game) {
         document.writeln(
             "<p id=\"" + game.id + "\"><span>&#10004;</span> <a href=\"" + game.link + "\">" + game.name + "</a></p>");
     });
 
+    // Add CSV header
     document.writeln("<pre>");
-
     var header = ["Game", "Steam link", "Game website", "Developer", "Publisher"];
     header = header.map(wrapInQuotes); // wrap string in quotes
     document.writeln(header.join());
@@ -31,22 +38,39 @@ function main() {
     window.addEventListener("storage", storageCallback);
 }
 
-function wrapInQuotes(str) {
-    return '"' + str + '"'
-}
-
+//noinspection JSValidateJSDoc (WebStorm doens't know about StorageEvent)
+/**
+ * Called when readPage writes game info in localStorage.
+ * Reads the new game info, updates the list of links, and adds the info to the CSV.
+ *
+ * @param {StorageEvent} event - A StorageEvent containing info about a game.
+ */
 function storageCallback(event) {
+    //noinspection JSUnresolvedVariable (Webstorm doesn't know about StorageEvent.key)
     var paragraph = $("p#" + event.key);
     paragraph.getElementsByTagName("span")[0].style.visibility = "visible"; //turn on the green checkmark
 
+    // Style the link
     var link = paragraph.getElementsByTagName("a")[0];
     link.style.color = "gray";
     link.style.textDecoration = "line-through";
 
+    //noinspection JSUnresolvedVariable (Webstorm doesn't know about StorageEvent.newValue)
     var gameInfo = JSON.parse(event.newValue);
+    // Write a CSV row
     var row = [gameInfo.name, gameInfo.steamLink, gameInfo.website, gameInfo.developer, gameInfo.publisher];
     row = row.map(wrapInQuotes);
     document.writeln(row.join());
+}
+
+/**
+ * Wraps a string in double quotes.
+ *
+ * @param {string} str - A string to wrap in quotes.
+ * @returns {string} - The string, wrapped in quotes.
+ */
+function wrapInQuotes(str) {
+    return '"' + str + '"'
 }
 
 main();
